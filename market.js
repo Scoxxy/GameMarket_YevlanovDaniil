a986b8447ca7213ed42afb10a5b357d41fdbcc3d
Scoxxy <158029869+Scoxxy@users.noreply.github.com>
2024-05-08 11:10:12 +0300

    pushe

--- market.js
+++ market.js
@@ -1,9 +1,16 @@
-console.log('Perevirka')
+console.log("Перевірка підключення файлу скриптів js");
 
 let itemsDiv = document.getElementById("items");
 
 if (itemsDiv) {
     console.log(itemsDiv)
+    for (let i = 0; i < 4; i++) {
+        itemsDiv.innerHTML += `<div class = "item">${i + 1}</div>`
+    }
+    console.log('Поле classlist: ', itemsDiv.classList)
+    console.log('Поле id:', itemsDiv.id)
+    console.log('Поле clientWidth:', itemsDiv.clientWidth)
+    console.log('Поле innerHTML:', itemsDiv.innerHTML)
 } else {
-    console.log("Блок не знайдено")
+    console.log('Блок товарів не знайдено')
 }
