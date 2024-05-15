8e5bd5eb1fedf6f5bb0d02f8c4460ddde0d0ba26
Scoxxy <158029869+Scoxxy@users.noreply.github.com>
2024-05-15 08:34:20 +0300

    124

--- market.js
+++ market.js
@@ -1,16 +1,29 @@
-console.log("Перевірка підключення файлу скриптів js");
+//console.log("Перевірка підключення файлу скриптів js");
+let itemsArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумулятивний оприскувач 12 N',
+    'Газонокосарка 430',
+    'Електричний тример 130',
+    'Електрична газонокосарка 320',
+    'Акумулятивний оприскувач 12 E ',
+]
 
 let itemsDiv = document.getElementById("items");
 
 if (itemsDiv) {
-    console.log(itemsDiv)
-    for (let i = 0; i < 4; i++) {
-        itemsDiv.innerHTML += `<div class = "item">${i + 1}</div>`
-    }
-    console.log('Поле classlist: ', itemsDiv.classList)
-    console.log('Поле id:', itemsDiv.id)
-    console.log('Поле clientWidth:', itemsDiv.clientWidth)
-    console.log('Поле innerHTML:', itemsDiv.innerHTML)
-} else {
+    itemsArray.forEach((items)=>{
+        itemsDiv.innerText += `<div class = "items">${items}</div>`
+    })
+} 
+else {
     console.log('Блок товарів не знайдено')
 }
+
+
+//itemsArray = itemsArray.sort()
+
+//for (let i = 0; i < itemsArray.length; i++){
+ //   console.log(i + '-й елемент: ',itemsArray[i])
+//}
