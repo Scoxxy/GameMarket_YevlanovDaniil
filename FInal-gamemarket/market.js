363dfb24dd81eb9edea28783e371ea96247bfe7f
Scoxxy <158029869+Scoxxy@users.noreply.github.com>
2024-05-22 11:01:07 +0300

    34573

---
+++ FInal-gamemarket/market.js
@@ -0,0 +1,355 @@
+
+let itemsArray = [
+    {
+        email: "23a_bam@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Боровий Артур",
+        project: "Дід проти москалів",
+        links: {
+            github: "https://github.com/23a-bam/Phaser2ndGame",
+            game: "https://23a-bam.github.io/Phaser2ndGame/",
+            drive: "https://drive.google.com/drive/folders/1A12wx0lfNj7Aef9KAw2CFMaDwhWM8V4Q?usp=sharing",
+            form: "https://forms.gle/sS7tuJWJuGDjmHB98",
+            gameMarketGithub: "https://github.com/23a-bam/GameMarket_Borovyi",
+            gameMarket: "https://23a-bam.github.io/GameMarket_Borovyi/",
+            market: "https://23a-bam.github.io/GameMarket_Borovyi/market.html"
+        }
+    },
+    {
+        email: "23a_gki@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Геча Кирило",
+        project: "Магазин UkraineRog",
+        links: {
+            github: "https://github.com/Kyrylooo/GameMarket_Surname",
+            settings: "https://github.com/Kyrylooo/GameMarket_Surname/settings/pages",
+            test: "https://kyrylooo.github.io/Test1/",
+            githubTest: "https://github.com/Kyrylooo/Test1"
+        }
+    },
+    {
+        email: "23a_gmo@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Громова Марина",
+        project: "",
+        links: {
+            github: "https://github.com/Marina745/Phaser2ndGame",
+            game: "https://marina745.github.io/Phaser2ndGame/",
+            drive: "https://drive.google.com/drive/folders/1h8T6v1Yt0wkzkiI72lSMMZndaidptT6o",
+            form: "https://forms.gle/4tsVxrHtBpkLLmdc6",
+            gameMarketJs: "https://github.com/Marina745/GameMarket_Hromova/blob/main/market.js",
+            market: "https://marina745.github.io/GameMarket_Hromova/market.html"
+        }
+    },
+    {
+        email: "23a_gayu@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Гуменюк Аріна",
+        project: "",
+        links: {
+            github: "https://github.com/NoraBlank/-Phaser2ndGame?authuser=0",
+            game: "https://norablank.github.io/-Phaser2ndGame/?authuser=0",
+            document: "https://docs.google.com/document/d/1ESxlcNrGJPWx--NdlFbMdZJNQ3mHWVKQ-HktIVOJBVY/edit?usp=sharing",
+            form: "https://forms.gle/rNroPXhu2mFSxcHx8"
+        }
+    },
+    {
+        email: "23a_dii@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Демидчук Ілля",
+        project: "",
+        links: {
+            github: "https://github.com/zxcursed2008/Phaser2ndGame",
+            game: "https://zxcursed2008.github.io/Phaser2ndGame/",
+            drive: "https://drive.google.com/drive/folders/1BfaZ86pNTA3FO_rOn196XXKOLYNQQ81E",
+            form: "https://forms.gle/xs7rgVauTR9ryuNc7",
+            gameMarketGithub: "https://github.com/zxcursed2008/GameMarket_Demydchuk/"
+        }
+    },
+    {
+        email: "23a_dyem@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Деркач Єлизавета",
+        project: "Царівна жаба",
+        links: {
+            github: "https://github.com/liska228/Phaser2ndGame",
+            game: "https://liska228.github.io/Phaser2ndGame/",
+            drive: "https://drive.google.com/drive/folders/1uFrc-z8MnRAFvR7sPNM1NnN800TJOUfr",
+            form: "https://forms.gle/XDzvrjzJhjq758XFA",
+            gameMarketGithub: "https://github.com/liska228/GameMarket_Derkach-",
+            gameMarket: "https://liska228.github.io/GameMarket_Derkach-/",
+            market: "https://liska228.github.io/GameMarket_Derkach-/market.html"
+        }
+    },
+    {
+        email: "23a_yedv@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Євланов Данііл",
+        project: "Пригоди Свинки Шмеппи 2",
+        links: {
+            github: "https://github.com/Scoxxy/Phaser2ndGame",
+            game: "https://scoxxy.github.io/Phaser2ndGame/",
+            drive: "https://drive.google.com/drive/folders/1IzTICIYe3e_rj81-ihjd3oDOz6xfSYQR?usp=drive_link",
+            form: "https://forms.gle/vWZ2hn9MopmMy5fc7",
+            gameMarketGithub: "https://github.com/Scoxxy/GameMarket_YevlanovDaniil",
+            gameMarket: "https://scoxxy.github.io/GameMarket_YevlanovDaniil/",
+            market: "https://scoxxy.github.io/GameMarket_YevlanovDaniil/market.html"
+        }
+    },
+    {
+        email: "23a_bks@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Катя Булич",
+        project: "Червона відьма",
+        links: {
+            github: "https://github.com/katyablchh/Phaser2ndGame",
+            game: "https://katyablchh.github.io/Phaser2ndGame/",
+            drive: "https://drive.google.com/drive/folders/1bNoYv5ChnzJTrIq0fjekfbqMNjz4SZ40?usp=sharing",
+            form: "https://forms.gle/dxTCp9vfCCgZwyy97",
+            gameMarketGithub: "https://katyablchh.github.io/Phaser2ndGame/",
+            gameMarket: "https://katyablchh.github.io/Phaser2ndGame/",
+            market: "https://github.com/katyablchh/GameMarket_Bulych"
+        }
+    },
+    {
+        email: "23a_kai@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Ковальов Андрій",
+        project: "They alive",
+        links: {
+            github: "https://github.com/Andre00785/Phaser1stGame",
+            settings: "https://github.com/Andre00785/Phaser1stGame/settings/pages",
+            document: "https://docs.google.com/document/d/1D1bRBFti1aHsr-ZAqkfDkK9tdEeOpkM26E9qyWCE8O0/edit?usp=sharing",
+            form: "https://forms.gle/i7YyjwzaHp7QvXei8",
+            gameMarketGithub: "https://github.com/Andre00785/Game_Market_Kovalev",
+            gameMarket: "https://andre00785.github.io/Game_Market_Kovalev/",
+            market: "https://andre00785.github.io/Game_Market_Kovalev/market.html"
+        }
+    },
+    {
+        email: "23a_kyuk@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Кожанов Юрій",
+        project: "Catventure",
+        links: {
+            github: "https://github.com/kyuk278937/Catventure",
+            drive: "https://drive.google.com/drive/folders/1GM-292zUzJSJlIB4-4_CIoOsHalliizh?usp=sharing",
+            form: "https://forms.gle/NN9okEBmpsLw3AmX8",
+            gameMarketGithub: "https://github.com/kyuk278937/GameMarket",
+            gameMarket: "https://kyuk278937.github.io/GameMarket/",
+            market: "https://kyuk278937.github.io/-HomeAppliancesStore/"
+        }
+    },
+    {
+        email: "23a_kmo@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Козлюк Марина",
+        project: "Пригоди червоної шапочки",
+        links: {
+            github: "https://github.com/Marykozl/Phaser2ndGame",
+            game: "https://marykozl.github.io/Phaser2ndGame/",
+            drive: "https://drive.google.com/drive/folders/1AZzV2iit4QmQfotZM68HmWo4OjG-5ikO",
+            form: "https://forms.gle/PdXpRXZrbn9ujeMH7",
+            gameMarketGithub: "https://github.com/Marykozl/GameMarket_Kozlyuk?hl=ru&authuser=0",
+            gameMarket: "https://marykozl.github.io/GameMarket_Kozlyuk/",
+            market: "https://marykozl.github.io/GameMarket_Kozlyuk/market.html"
+        }
+    },
+    {
+        email: "23a_kia@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Кравчук Іван",
+        project: "Пригоди Свиноти",
+        links: {
+            github: "https://github.com/maskenissen1/Phaser2ndGame-1",
+            game: "https://maskenissen1.github.io/Phaser2ndGame-1/",
+            drive: "https://drive.google.com/drive/folders/1_qC1jSGlqtfs16m1_nriWvomLg51Ah25",
+            form: "https://forms.gle/S2YEZ1dn7tYTkrbF6",
+            gameMarketGithub: "https://github.com/maskenissen1/GameMarket_Kravchuk",
+            gameMarket: "https://maskenissen1.github.io/GameMarket_Kravchuk/",
+            market: "https://maskenissen1.github.io/GameMarket_Kravchuk/market.html"
+        }
+    },
+    {
+        email: "23a_lsa@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Лазаренко Софія",
+        project: "Mavka",
+        links: {
+            github: "https://github.com/SofiaLazarenko/Phaser2ndGame?authuser=0",
+            game: "https://sofialazarenko.github.io/Phaser2ndGame/?authuser=0",
+            document: "https://docs.google.com/document/d/1Dqe-8TcUA1YLj7mG-2IMBxHsPmlhkwfkEfrOTAb9Nvs/edit?usp=classroom_web&authuser=0",
+            form: "https://forms.gle/PgJv2LH1ZJjjS29V8",
+            gameMarketGithub: "https://github.com/SofiaLazarenko/GameMarket_Lazarenko?authuser=0",
+            gameMarket: "https://sofialazarenko.github.io/GameMarket_Lazarenko/index.html",
+            market: "https://sofialazarenko.github.io/GameMarket_Lazarenko/n/market.html?authuser=0"
+        }
+    },
+    {
+        email: "23a_kiv@liceum.ztu.edu.ua",
+        class: "10-А 1",
+        name: "Кайданюк Ілля",
+        project: "Пригоди Свинки Шмеппи 3",
+        links: {
+            github: "https://github.com/Nefenix/Phaser2ndGame",
+            game: "https://nefenix.github.io/Phaser2ndGame/",
+            drive: "https://drive.google.com/drive/folders/1xlqWdx6uMj_5XWDlfl4EYZtAD6A8LGMq?usp=drive_link",
+            form: "https://forms.gle/e4camX4k2MZvK9qf7",
+            gameMarketGithub: "https://github.com/Nefenix/GameMarket_Kaidaniuk",
+            gameMarket: "https://nefenix.github.io/GameMarket_Kaidaniuk/",
+            market: "https://nefenix.github.io/GameMarket_Kaidaniuk/market.html"
+        }
+    }
+];
+  
+]
+// Отримання елементу з індифікатором items
+let itemsDiv = document.getElementById("items");
+// Перевірка існування знайденого блоку 
+if (itemsDiv) { 
+    // Створення блоків по кількості елементів масиву
+    itemsArray.forEach((item, index) => {
+        // Виводимо на веб-сторінку елемент масиву в блок з класом item
+        itemsDiv.innerHTML += `
+    
+        <div class="item">
+            <div class="bober">
+                <div class="item-title">${item.email}</div>
+                
+            </div>
+            
+
+            <p><img src="${item.Group}" " class="item-image"></p>
+
+            <div class="parts-pay">
+            <div><img src="img-market/mono-lapka.png" alt="">6</div>
+            <div><img src="img-market/pb.png" alt="">8</div>
+            </div>
+
+            <div class="prise">
+            <div><span>${item.stud}</span><sup>грн</sup></div>
+            <div><span>${item.discountPrice}</span><sup>грн</sup></div>
+            
+            </div>
+
+            <div class="prise bonus">
+            <div>ціна за купоном</div>
+            <div><span>${item.couponPrice}</span><sup>грн</sup></div>
+            </div>
+        </div>`
+    
+
+
+      
+    });
+} else {
+    // Вивід повідомлення про не знайдений блок 
+    console.log('Блок товарів не знайдено');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Виведення елементів відсортованого масиву
+// itemsArray.sort().forEach((item,index) =>{
+//     console.log(index + '-й елемент:',item)
+// })
+// //Сортування масиву
+// itemsArray = itemsArray.sort()
+// // Виведення в консоль масиву
+// console.log(itemsArray)
+// //Виведення в консоль елементів масиву
+// for(let i = 0; i<itemsArray.length; i++){
+//     console.log(itemsArray[i])
+// }
+// //Виведення в коносль номерів та значень елементів масиву
+// for(let i = 0; i < itemsArray.length; i++){
+//     console.log(i +'-й елемент: ', itemsArray[i])
+// }
+// Початковий показ вчителів
+// displayTeachers(itemsArray);
+
+// function displayTeachers(teachers) {
+//     let sortedTeachers = teachers.slice(); // Клонування масиву
+//     itemsDiv.innerHTML = ''; // Очистити вміст контейнера
+
+//     sortedTeachers.forEach((teacher, index) => {
+//         itemsDiv.innerHTML +=
+//             `<div class="item">
+//                 <h2>Вчитель № ${index + 1} з ${sortedTeachers.length}</h2>
+//                 <p>${teacher.lastName} ${teacher.firstName}</p>
+//                 <p><img src="${teacher.photo}" alt="${teacher.lastName} ${teacher.firstName}" class="item-image"></p>
+//                 <p>Вік: ${teacher.age} </p>
+//                 <p>Предмет: ${teacher.subject} </p>
+//                 <p>Освіта: ${teacher.education} </p>
+//                 <p>Звання: ${teacher.rank} </p>
+//                 <p><a href="${teacher.url}" target="_blank">Профіль</a></p>
+//             </div>`;
+//     });
+// }
+
+// function sortByAge() {
+//     let sortedByAge = itemsArray.slice().sort((a, b) => parseInt(a.age) - parseInt(b.age));
+//     displayTeachers(sortedByAge);
+// }
+
+// function sortByLastName() {
+//     let sortedByLastName = itemsArray.slice().sort((a, b) => a.lastName.localeCompare(b.lastName));
+//     displayTeachers(sortedByLastName);
+// }
+
+// function sortBySubject() {
+//     let sortedBySubject = itemsArray.slice().sort((a, b) => a.subject.localeCompare(b.subject));
+//     displayTeachers(sortedBySubject);
+// }
